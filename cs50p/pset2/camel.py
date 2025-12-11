07f58b73d716e5e984250ec9c10f7914a3cab25d
tahsinttalha2 <tahsinttalha@gmail.com>
2025-12-11 17:14:04 +0600

    converts camelCase to snake_case

---
+++ cs50p/pset2/camel.py
@@ -0,0 +1,16 @@
+s = input("camelCase: ")
+string = []
+
+a = 0
+b = 0
+for i in s:
+    if i.isupper() == True:
+        string.append(s[a:b])
+        a = b
+        print(a)
+    b += 1
+string.append(s[a:])
+
+new_string = "_".join(string)
+
+print(new_string.lower())
